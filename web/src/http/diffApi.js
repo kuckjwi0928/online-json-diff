deb02d84660ffe133efd01ac3c678a64f93f52ab
kuckjwi <rnrghks09@gmail.com>
2022-11-14 13:51:46 +0900

    add uri encode

--- web/src/http/diffApi.js
+++ web/src/http/diffApi.js
@@ -1,7 +1,7 @@
 import client from "./client";
 
 export async function getV1DiffTarget(originURL, compareURL, headers) {
-  return await client().get(`/v1/diff-target?originURL=${originURL}&compareURL=${compareURL}&headerKeys=${Object.keys(headers).join(',')}`, {
+  return await client().get(`/v1/diff-target?originURL=${encodeURIComponent(originURL)}&compareURL=${encodeURIComponent(compareURL)}&headerKeys=${Object.keys(headers).join(',')}`, {
     headers: {
       ...headers,
     }
