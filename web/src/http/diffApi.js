826bf22af6769c57ca6ce235ad636579c2b133de
kuckjwi <rnrghks09@gmail.com>
2022-11-14 18:30:46 +0900

    fix: fixed json send error

--- web/src/http/diffApi.js
+++ web/src/http/diffApi.js
@@ -3,7 +3,7 @@ import client from "./client";
 export async function getV1DiffTarget(originURL, compareURL, method, bodies, headers) {
   const encodeOrigin = encodeURIComponent(originURL)
   const encodeCompare = encodeURIComponent(compareURL)
-  const bodyJSON = encodeURIComponent(JSON.stringify(bodies))
+  const bodyJSON = encodeURIComponent(bodies)
   return await client().get(`/v1/diff-target?originURL=${encodeOrigin}&compareURL=${encodeCompare}&method=${method}&bodyJSON=${bodyJSON}&headerKeys=${Object.keys(headers).join(',')}`, {
     headers: {
       ...headers,
